10ee6672a32bc795a08caed43b14f62ab0763522
Jerome Mayran <jmayran@aemis.fr>
2026-05-11 17:12:34 +0200

    fix php : translation blog

---
+++ modules/tvcmsblogdisplayposts/translations/fr.php
@@ -0,0 +1,9 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{tvcmsblogdisplayposts}prestashop>tvcmsblogdisplayposts_left-bk_f1e98a26384a5e804c97236b60cebfae'] = 'Voir toutes les infos';
+$_MODULE['<{tvcmsblogdisplayposts}prestashop>tvcmsblogdisplayposts_left_f1e98a26384a5e804c97236b60cebfae'] = 'Voir toutes les infos';
+$_MODULE['<{tvcmsblogdisplayposts}prestashop>tvcmsblogdisplayposts_home_f1e98a26384a5e804c97236b60cebfae'] = 'Voir toutes les infos';
+$_MODULE['<{tvcmsblogdisplayposts}prestashop>tvcmsblogdisplayfooterposts_a5c22ff3efbced22c34bd7b7dd482a6d'] = 'Voir toutes les infos';
+
